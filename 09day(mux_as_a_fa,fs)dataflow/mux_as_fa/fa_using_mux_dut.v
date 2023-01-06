4fd2c9732a965e8181b6d5afb64eb66980d03ece
Nitin Dinnu <68476175+nitindinnu@users.noreply.github.com>
2023-01-06 12:23:17 +0530

    .

--- 09day(mux_as_a_fa,fs)dataflow/mux_as_fa/fa_using_mux_dut.v
+++ 09day(mux_as_a_fa,fs)dataflow/mux_as_fa/fa_using_mux_dut.v
@@ -2,6 +2,7 @@ module fa_implementation_in_mux_dut (
     input a,b,c,output sum,carry
 );
     assign sum= b?(a?c:~c):(a?~c:c);
-    //assign =~a&~b&0 | ~a&b&c | a&~b&c | a&b
+    //assign sum=~a&~b&c |~a&b&~c |a&~b&c|a&b&c;
     assign carry= b?(a?1:c):(a?c:0);
+    //assign carry = ~a&~b&0 | ~a&b&c | a&~b&c | a&b&1;
 endmodule
